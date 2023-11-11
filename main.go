bd0487ddb36203a58b76fdc7b72d9e2502775326
blazingh <hadibaalbaki@skiff.com>
2023-11-11 22:54:12 +0300

    added .env and updated logging messages

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/joho/godotenv"
 )
 
 type File struct {
@@ -26,42 +28,65 @@ type Tunnel struct {
 var files = map[int]File{}
 
 func main() {
+	// check if .env file exists
+	if _, err := os.Stat(".env"); os.IsNotExist(err) {
+		panic(".env file not found")
+	}
 
-	// start a routine to handle http
+	// check if id_rsa file exists
+	if _, err := os.Stat("id_rsa"); os.IsNotExist(err) {
+		panic("id_rsa file not found, you can run `ssh-keygen -t rsa -b 4096 -f id_rsa -q -N ''`")
+	}
+
+	// load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	sshPort := os.Getenv("SSH_PORT")
+	if sshPort == "" {
+		sshPort = "2222"
+	}
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8888"
+	}
+
+	// start http server with a go routine
 	go func() {
 		http.HandleFunc("/file", handleHttp)
-		log.Fatal(http.ListenAndServe(":8888", nil))
+		fmt.Printf("HTTP Listening on port %s\n", httpPort)
+		log.Fatal(http.ListenAndServe(":"+httpPort, nil))
 	}()
 
 	ssh.Handle(hadleSsh)
 
 	// start ssh server
-	log.Fatal(ssh.ListenAndServe(":2222", nil, ssh.HostKeyFile("./id_rsa")))
+	fmt.Printf("SSH Listening on port %s\n", sshPort)
+	log.Fatal(ssh.ListenAndServe(":"+sshPort, nil, ssh.HostKeyFile("./id_rsa")))
 }
 
 func hadleSsh(s ssh.Session) {
-	fmt.Printf("session opened: %s\n", s.User())
+
+	fmt.Printf("%s : session opened\n", s.User())
+
+	// close the session when the client disconnect
+	defer func() {
+		s.Close()
+	}()
 
 	args := s.Command()
 	if len(args) == 0 {
 		s.Write([]byte("no arguments provided\n"))
-		fmt.Printf("-- error : no arguments provided \n")
 		return
 	}
 
 	nameCmd := strings.Split(args[0], "=")
 	if len(nameCmd) < 2 || nameCmd[0] != "name" || nameCmd[1] == "" {
 		s.Write([]byte("invalid name command\n"))
-		fmt.Printf("-- error : invalid name command \n")
 		return
 	}
-
-	// end function when the client disconnect
-	go func() {
-		<-s.Context().Done()
-		fmt.Printf("-- message: session closed by client \n")
-		return
-	}()
 
 	// create a new tunnel
 	id := rand.Intn(math.MaxInt)
@@ -71,11 +96,31 @@ func hadleSsh(s ssh.Session) {
 	}
 	files[id] = file
 
+	// delete the file when the connection is closed
+	defer func(id int) {
+		if _, ok := files[id]; ok {
+			delete(files, id)
+		}
+	}(id)
+
+	// delete the file if the connection is closed
+	go func(id int) {
+		<-s.Context().Done()
+		if _, ok := files[id]; ok {
+			fmt.Printf("%s : closing tunnel %d\n", s.User(), id)
+			delete(files, id)
+		}
+	}(id)
+
 	s.Write([]byte("tunnel id: " + strconv.Itoa(id) + "\n"))
-	fmt.Printf("-- message: tunnel is ready: %d\n", id)
+	fmt.Printf("%s : tunnel is ready: %d\n", s.User(), id)
 
 	// wait for the receiver to connect
 	tunnel := <-file.tunnel
+	// close the tunnel when the connection is closed
+	defer func() {
+		close(tunnel.doneChan)
+	}()
 
 	// send the file
 	_, err := io.Copy(tunnel.writer, s)
@@ -84,15 +129,13 @@ func hadleSsh(s ssh.Session) {
 	}
 
 	// close the tunnel and the connection
-	close(tunnel.doneChan)
-	s.Write([]byte("file sent\n"))
-	fmt.Printf("-- message: file sent \n")
-	s.Close()
+	s.Write([]byte("file received by receiver\n"))
+	fmt.Printf("%s : done sending file \n", s.User())
 }
 
 func handleHttp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	idstr := r.URL.Query().Get("id")
