e78b339de563ae2bf0c5bbb762d746b520fa94d3
blazingh <hadibaalbaki@skiff.com>
2023-11-14 14:01:01 +0300

    added http handler packaeg

---
+++ httpHandler/httpHandler.go
@@ -0,0 +1,62 @@
+package httpHandler
+
+import (
+	"fmt"
+	"github/blazingh/go-stream/sshHandler"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func Start() {
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8888"
+	}
+
+	fs := http.FileServer(http.Dir("./public"))
+
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
+	http.HandleFunc("/", handleLanding)
+
+	http.HandleFunc("/file", handleFile)
+
+	fmt.Printf("HTTP Listening on port %s\n", httpPort)
+	log.Fatal(http.ListenAndServe(":"+httpPort, nil))
+}
+
+func handleFile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	idstr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		w.Write([]byte("invalid id"))
+		return
+	}
+
+	openTunnel, ok := sshHandler.OpenedTunnels[id]
+	if !ok {
+		w.Write([]byte("not found"))
+		return
+	}
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+openTunnel.FileName)
+
+	// send when writer to the ssh
+	openTunnel.Writer <- w
+	// wait for the writer to close
+	<-openTunnel.DoneChan
+}
+
+func handleLanding(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	tmpl := template.Must(template.ParseFiles("template/home.html"))
+	tmpl.Execute(w, nil)
+}
--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
+
+	"github/blazingh/go-stream/httpHandler"
+	"github/blazingh/go-stream/sshHandler"
 
 	"github.com/joho/godotenv"
-	"github/blazingh/go-stream/sshHandler"
 )
 
-func main() {
+func init() {
 	// check if .env file exists
 	if _, err := os.Stat(".env"); os.IsNotExist(err) {
 		panic(".env file not found")
@@ -28,57 +26,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
 
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8888"
-	}
+func main() {
 
-	// start http server with a go routine
-	go func() {
-		// set up a file server
-		fs := http.FileServer(http.Dir("./public"))
-		http.Handle("/public/", http.StripPrefix("/public/", fs))
-
-		http.HandleFunc("/home", handleHome)
-
-		http.HandleFunc("/file", handleHttp)
-
-		fmt.Printf("HTTP Listening on port %s\n", httpPort)
-		log.Fatal(http.ListenAndServe(":"+httpPort, nil))
-	}()
+	go httpHandler.Start()
 
 	sshHandler.Start()
-}
-
-func handleHttp(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-	idstr := r.URL.Query().Get("id")
-	id, err := strconv.Atoi(idstr)
-	if err != nil {
-		w.Write([]byte("invalid id"))
-		return
-	}
-
-	openTunnel, ok := sshHandler.OpenedTunnels[id]
-	if !ok {
-		w.Write([]byte("not found"))
-		return
-	}
-
-	w.Header().Set("Content-Disposition", "attachment; filename="+openTunnel.FileName)
-
-	// send when writer to the ssh
-	openTunnel.Writer <- w
-	// wait for the writer to close
-	<-openTunnel.DoneChan
-}
-
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	tmpl := template.Must(template.ParseFiles("template/home.html"))
-	tmpl.Execute(w, nil)
 }
