9579e7411c295e235878b2035be4eb40d755a22b
blazingh <hadibaalbaki@skiff.com>
2023-11-15 17:09:05 +0300

    added tunnel info api

--- httphandler/httpHandler.go
+++ httphandler/httpHandler.go
@@ -1,13 +1,15 @@
 package httphandler
 
 import (
+	"encoding/json"
 	"fmt"
-	sshHandler "github.com/blazingh/beampaw/sshhandler"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+
+	sshHandler "github.com/blazingh/beampaw/sshhandler"
 )
 
 func Start() {
@@ -27,8 +29,55 @@ func Start() {
 
 	http.HandleFunc("/download", handleDownload)
 
+	http.HandleFunc("/api/tunnel", handleGetTunnel)
+
 	fmt.Printf("HTTP Listening on port %s\n", httpPort)
 	log.Fatal(http.ListenAndServe(":"+httpPort, nil))
+}
+
+func handleGetTunnel(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	idstr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("invalid id"))
+		return
+	}
+
+	openTunnel, ok := sshHandler.OpenedTunnels[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("not found"))
+		return
+	}
+
+	data := struct {
+		TunnelId    string `json:"tunnelId"`
+		TunnelType  string `json:"tunnelType"`
+		FileName    string `json:"fileName"`
+		DownloadURL string `json:"downloadUrl"`
+	}{
+		TunnelId:    idstr,
+		TunnelType:  "ssh",
+		FileName:    openTunnel.FileName,
+		DownloadURL: os.Getenv("WEB_URL") + "/download?id=" + idstr,
+	}
+
+	jsonResp, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
+
+	return
 }
 
 func handleFile(w http.ResponseWriter, r *http.Request) {
@@ -55,6 +104,8 @@ func handleFile(w http.ResponseWriter, r *http.Request) {
 	openTunnel.Writer <- w
 	// wait for the writer to close
 	<-openTunnel.DoneChan
+
+	return
 }
 
 func handleLanding(w http.ResponseWriter, r *http.Request) {
@@ -63,35 +114,17 @@ func handleLanding(w http.ResponseWriter, r *http.Request) {
 	err := tmpl.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		w.Write([]byte(err.Error()))
+		return
 	}
 }
 
 func handleDownload(w http.ResponseWriter, r *http.Request) {
-	idstr := r.URL.Query().Get("id")
-	id, err := strconv.Atoi(idstr)
-	if err != nil {
-		w.Write([]byte("invalid id"))
-		return
-	}
-
-	openTunnel, ok := sshHandler.OpenedTunnels[id]
-	if !ok {
-		w.Write([]byte("not found"))
-		return
-	}
-
-	data := struct {
-		FileName    string
-		DownloadURL string
-	}{
-		FileName:    openTunnel.FileName,
-		DownloadURL: os.Getenv("WEB_URL") + "/file?id=" + idstr,
-	}
-
 	w.Header().Set("Content-Type", "text/html")
+
 	tmpl, _ := template.New("base").ParseFiles("template/base.html", "template/download.html")
-	err = tmpl.ExecuteTemplate(w, "base", data)
+	err := tmpl.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		w.Write([]byte(err.Error()))
+		return
 	}
 }
